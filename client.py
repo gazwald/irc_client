9753561c079bc451d2f24a34fa3501e43cd7ba97
Gary Brandon <gary.brandon@gmail.com>
2014-10-19 19:28:21 +1100

    Added basic socket

--- client.py
+++ client.py
@@ -1,5 +1,17 @@
 __author__ = 'user'
 
+import socket
+
+
+def main():
+    remote_host = 'irc.freenode.org'
+    remote_port = 6667
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((remote_host, remote_port))
+    data = s.recv(1024)
+    s.close()
+    print('Received: ', repr(data))
+
+
 if __name__ == "__main__":
-    print("Hello world")
-    print("Hello world")
+    main()
