edfe8269e38a427c984b413143f220af274e87f9
Gazwald <me@gazwald.com>
2016-10-15 20:21:41 +1100

    Moving the client based code into a class. Massive refactoring and some improved error handling in this commit. Added --user arg

--- client.py
+++ client.py
@@ -3,60 +3,80 @@ import socket
 import threading
 import logging
 import argparse
+import sys
 
 
 logging.basicConfig(filename='irc_client.log',level=logging.DEBUG)
 
 
-def creat_socket(remote_host, remote_port):
-    global s
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((remote_host, remote_port))
-
-
-def send_data(message):
-    message = bytes(message + '\r\n', 'ascii', errors='skip')
-
-
-    sent_total = 0
-    while sent_total < len(message):
-        sent = s.send(message[sent_total:])
-        sent_total += sent
-
-    return True
+class Client:
+    def __init__(self, remote_host, remote_port=6667):
+        """
+        Create a socket and connect to the server.
+        """
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.s.connect((remote_host, remote_port))
+        except socket.error:
+            sys.exit("Socket error, bailing...")
+        else:
+            self.start_polling()
 
 
-def recv_data(data_raw):
-    data_ascii = data_raw.decode('ascii')
-    return data_ascii
+    def start_polling(self):
+        self.poller = threading.Thread(target=self.poll)
+        self.poller.daemon = True
+        self.poller.start()
 
 
-def format_data(data):
-    logging.debug(data)
+    def send(self, message):
+        message = bytes(message + '\r\n', 'ascii', errors='skip')
 
-    if 'PRIVMSG' in data:
-        split_data = data.split(' ')
-        channel = split_data[2]
-        user = split_data[0].split('!')[0]
-        user = user.strip(':')
-        message = split_data[3]
-        message = message.strip(':')
-        print('%s - <%s>: %s' % (channel, user, message))
-    elif 'PING' in data:
-        send_data(data.replace('PING', 'PONG'))
-    else:
-        print(data)
+        sent_total = 0
+        while sent_total < len(message):
+            sent = s.send(message[sent_total:])
+            sent_total += sent
 
 
-def poll():
-    while True:
-        format_data(recv_data(s.recv(1024)))
+    def format_data(self, data):
+        logging.debug(data)
+
+        if 'PRIVMSG' in data:
+            split_data = data.split(' ')
+            channel = split_data[2]
+            user = split_data[0].split('!')[0]
+            user = user.strip(':')
+            message = split_data[3]
+            message = message.strip(':')
+            print('%s - <%s>: %s' % (channel, user, message))
+        elif 'PING' in data:
+            self.send(data.replace('PING', 'PONG'))
+        else:
+            print(data)
+
+
+    def recv(self, data):
+        data_ascii = data.decode('ascii')
+        return data_ascii
+
+
+    def poll(self):
+        while True:
+            if self.poller.is_alive():
+                self.format_data(self.recv(self.s.recv(1024)))
+            else:
+                sys.exit("Poller died, bailing...")
+                break
+
+
+    def quit(self):
+        self.s.close()
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='Connect to an IRC server')
     parser.add_argument('--server', required=True)
+    parser.add_argument('--user', required=True)
 
     return parser.parse_args()
 
@@ -64,11 +84,7 @@ def get_args():
 def main():
     args = get_args()
 
-    creat_socket(remote_host=args.server, remote_port=6667)
-
-    recv = threading.Thread(target=poll)
-    recv.daemon = True
-    recv.start()
+    new_client = Client(remote_host=args.server)
 
     current_channel = None
 
@@ -77,7 +93,8 @@ def main():
         if '/' in value:
             if 'quit' in value.lower():
                 msg = 'QUIT :Client quit.'
-                send_data(msg)
+                new_client.send(msg)
+                new_client.quit()
                 break
             elif 'join' in value.lower():
                 channel = value.split(' ')[1]
@@ -85,25 +102,23 @@ def main():
                     current_channel = channel
 
                 msg = 'JOIN %s' % channel
-                send_data(msg)
+                new_client.send(msg)
             elif 'nick' in value.lower():
-                user = value.split(' ')[1]
-                msg = 'NICK %s' % user
-                send_data(msg)
-                msg = 'USER %s * *  : %s' % (user, user)
-                send_data(msg)
+                msg = 'NICK %s' % args.user
+                new_client.send(msg)
+                msg = 'USER %s * *  : %s' % (args.user, args.user)
+                new_client.send(msg)
             elif 'switch' in value.lower():
                 current_channel = value.split()[1]
 
             else:
                 # I don't know what you wanted to do
                 # Send it and hope for the best
-                send_data(value)
+                new_client.send(value)
         else:
             msg = 'PRIVMSG %s :%s' % (current_channel, value)
-            send_data(msg)
+            new_client.send(msg)
 
 
 if __name__ == "__main__":
     main()
-    s.close()
